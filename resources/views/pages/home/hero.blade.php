5640e37c011f6c28bc8e79fdd746f5308f3f305f
Weslley Richard <weslleyrichardc@gmail.com>
2022-09-02 21:51:39 -0300

    feat: Remove from home page the advanced search

--- resources/views/pages/home/hero.blade.php
+++ resources/views/pages/home/hero.blade.php
@@ -4,40 +4,4 @@
         <p class="text-xl font-light tracking-wide">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel sed in mauris, libero magna. Cursus urna sem odio nulla aliquam. Nascetur.</p>
         <x-button>Sobre Nós</x-button>
     </div>
-    <div class="min-w-fit max-w-fit h-fit bg-white px-4 shadow sm:rounded-md sm:overflow-hidden">
-        <form action="#" method="POST">
-            <div class="pt-4 space-y-1">
-                <x-form.select>
-                    <option selected>Cidade</option>
-                    <option value="1">Praia Grande</option>
-                    <option value="2">São Vicente</option>
-                    <option value="3">Santos</option>
-                </x-form.select>                
-            </div>
-            <div class="grid grid-cols-2 gap-4">
-                <x-form.select>
-                    <option selected>Marca</option>
-                    <option value="1">Chevrolet</option>
-                    <option value="2">Fiat</option>
-                    <option value="3">Ford</option>
-                    <option value="4">Renault</option>
-                </x-form.select>
-                <x-form.select>
-                    <option selected>Modelo</option>
-                    <option value="1">Sandero</option>
-                    <option value="2">Zafira</option>
-                    <option value="3">Golf</option>
-                </x-form.select>
-            </div>
-            <label>Valor</label>
-            <div class="grid grid-cols-2 gap-4">
-                <x-form.input>De</x-form.input>
-                <x-form.input>Até</x-form.input>
-            </div>
-            <div class="flex gap-4 pb-4">
-                <x-button outline class="text-orange border-orange">Pesquisa Avançada</x-button>
-                <x-button>Pesquisar</x-button>
-            </div>
-        </form>
-    </div>
 </div>
