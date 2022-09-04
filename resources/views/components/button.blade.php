51d3fb1d2eaf2cbd7ecea0c6ff5f23efac924241
Weslley Richard <weslleyrichardc@gmail.com>
2022-09-04 01:18:11 -0300

    wip: auth pages

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,7 @@
-@props(['outline' => false])
+@props([
+    'outline' => false,
+    'type' => ''
+])
 
 @php
     $class = ['inline-flex', 'items-center', 'px-4', 'py-2', 'bg-orange', 'rounded-md', 'font-semibold', 'text-xs', 'uppercase', 'tracking-widest', 'focus:outline-none', 'focus:border-orange-dark', 'disabled:opacity-25', 'transition', 'ease-in-out', 'duration-150'];
@@ -8,6 +11,13 @@
                 : ' border border-transparent hover:bg-orange-light active:bg-orange-dark';
 @endphp
 
-<a {{ $attributes->class($class) }}>
-    {{ $slot }}
-</a>
+@if($type === "button")
+    <button {{ $attributes->class($class) }}>
+        {{ $slot }}
+    </button>
+@else
+    <a {{ $attributes->class($class) }}>
+        {{ $slot }}
+    </a>
+@endif
+
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -3,5 +3,24 @@
 @section('title', 'Login')
 
 @section('content')
-<h1>Login</h1>
+    <div class="container flex mx-auto py-32">
+        <div class="grid justify-items-center w-1/2">
+            <h1 class="my-3 text-2xl text-black uppercase font-semibold">Login</h1>
+
+            <form>
+                <x-form.input placeholder="CPF/CNPJ"></x-form.input>
+                <x-form.input placeholder="Senha" type="password"></x-form.input>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-button type="button">Login</x-button>
+                </div>
+            </form>
+        </div>
+        <div class="grid justify-items-center w-1/2 gap-4">
+            <h1 class="my-3 text-2xl text-black uppercase font-semibold">Cadastrar</h1>
+
+            <x-button :href="route('auth.register.user')">Cadastrar Usuário</x-button>
+            <x-button :href="route('auth.register.dealership')">Cadastrar Concessionária</x-button>
+        </div>
+    </div>
 @endsection
