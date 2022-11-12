e3771affcd46876f93e6222760664854c8d3e593
Weslley Richard <weslleyrichardc@gmail.com>
2022-11-12 15:14:43 -0300

    fix: update some bugs found

--- resources/views/pages/auth/register/dealership.blade.php
+++ resources/views/pages/auth/register/dealership.blade.php
@@ -12,14 +12,14 @@
                     <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
                         @csrf
 
-                        <x-form.input class="md:col-span-5" name="name" placeholder="Nome Fantasia" />
-                        <x-form.input class="md:col-span-5" placeholder="E-mail" type="email" />
-                        <x-form.input class="md:col-span-5" placeholder="CNPJ" />
-                        <x-form.input class="md:col-span-5" placeholder="Telefone" />
-                        <x-form.input class="md:col-span-3" placeholder="Endereço" />
-                        <x-form.input class="md:col-span-2" placeholder="Número" />
-                        <x-form.input class="md:col-span-3" placeholder="Complemento" />
-                        <x-form.input class="md:col-span-2" placeholder="CEP" />
+                        <x-form.input x-col="md:col-span-5" name="nome" placeholder="Nome Fantasia" />
+                        <x-form.input x-col="md:col-span-5" name="email" placeholder="E-mail" type="email" />
+                        <x-form.input x-col="md:col-span-5" name="cnpj" placeholder="CNPJ" />
+                        <x-form.input x-col="md:col-span-5" name="telefone" placeholder="Telefone" />
+                        <x-form.input x-col="md:col-span-3" name="endereço" placeholder="Endereço" />
+                        <x-form.input x-col="md:col-span-2" name="numero" placeholder="Número" />
+                        <x-form.input x-col="md:col-span-3" name="complemento" placeholder="Complemento" />
+                        <x-form.input x-col="md:col-span-2" name="cep" placeholder="CEP" />
 
                         <p class="md:col-span-5 my-3 text-md text-black">Leia nossos termos e condições <a href="#" class="text-orange">clicando aqui!</a></p>
                         <div class="md:col-span-5 inline-flex items-center">
--- resources/views/pages/car/search-box.blade.php
+++ resources/views/pages/car/search-box.blade.php
@@ -1,7 +1,7 @@
 <section id="search-box" class="bg-white border-x-4 border-y-8 border-y-blue-dark rounded-md md:w-96 h-auto md:h-max float-left mb-8">
     <div class="p-4 lg:col-span-2 text-black">
         <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-6">
-            <x-form.input class="md:col-span-6" name="name" placeholder="Pesquisar nome" />
+            <x-form.input x-col="md:col-span-6" name="name" placeholder="Pesquisar nome" />
             <x-form.select class="md:col-span-6 lg:col-span-3 mt-3" name="brand" placeholder="Marca">
                 <option value="">Marca</option>
             </x-form.select>
